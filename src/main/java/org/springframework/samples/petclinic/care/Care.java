7cb7623d23149b5254c8278b199a7b2aa03bf263
linto <linto@dircom.manten.us.es>
2022-01-21 11:22:24 +0100

    entrega examen

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,6 +2,16 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -9,8 +19,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Care {
+@Entity
+@Table(name = "cares")
+public class Care extends BaseEntity{
+	@NotNull
+	@Size(min = 5, max = 30)
+	@Column(name = "name", unique = true)
     String name;
+	
+	@NotNull
+	@Column(name = "description")
     String description;
+	
+	@NotNull
+	@NotEmpty
+	@ManyToMany(cascade = CascadeType.ALL)
     Set<PetType> compatiblePetTypes;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	Set<Care> incompatibleCares;
 }
